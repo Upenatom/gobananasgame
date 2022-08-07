40ca7b4e8dc590fe865396ba2038d6f75704d014
Randolph <randolph@Randolphs-Mini.home>
2022-08-06 21:41:06 -0500

    basic outline of games boxes

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*---- constants ----*/
+const numOfRounds = 3;
+const timerSolvePuzz = 500; //500ms
+/*---- app's state (variables) ----*/
+
+/*---- cached element references ----*/
+/*---- event listeners ----*/
+/*---- functions ----*/
