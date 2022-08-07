e5040d413333915d3d1f8317f6fa4665bcdbb7e9
Randolph <randolph@Randolphs-Mini.home>
2022-08-06 22:28:41 -0500

    prelim event listeners added

--- js/script.js
+++ js/script.js
@@ -4,16 +4,24 @@ const timerSolvePuzz = 500; //500ms
 /*---- app's state (variables) ----*/
 
 /*---- cached element references ----*/
-const instruct = document.getElementById("instruct");
-const p1name = document.getElementById("p1name");
-const p2name = document.getElementById("p2name");
-const p1points = document.getElementById("p1points");
-const p2points = document.getElementById("p2points");
-const p1solve = document.getElementById("p1solve");
-const p2solve = document.getElementById("p2solve");
-const gameboard = document.getElementById("gameboard");
-const letters = document.getElementById("letters");
-const spinner = document.getElementById("spinner");
-const spinbutt = document.getElementById("spinbutt");
+const roundEl = document.getElementById("round");
+const instructEl = document.getElementById("instruct");
+const p1NameEl = document.getElementById("p1name");
+const p2NameEl = document.getElementById("p2name");
+const p1PointsEl = document.getElementById("p1points");
+const p2PointsEl = document.getElementById("p2points");
+const p1SolveEl = document.getElementById("p1solve");
+const p2SolveEl = document.getElementById("p2solve");
+const gameBoardEl = document.getElementById("gameboard");
+const lettersEl = document.getElementById("letters");
+const spinnerEl = document.getElementById("spinner");
+const spinButtEl = document.getElementById("spinbutt");
+
 /*---- event listeners ----*/
+p1SolveEl.addEventListener("keypress");
+p2SolveEl.addEventListener("keypress");
+spinButtEl.addEventListener("click");
+spinButtEl.addEventListener("keypress");
+lettersEl.addEventListener("click");
+
 /*---- functions ----*/
