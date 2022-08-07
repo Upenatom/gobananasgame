80cbbd001a5157eaf5616eb925c96e289acea25f
Randolph <randolph@Randolphs-Mini.home>
2022-08-06 21:49:53 -0500

    cahed element references added

--- js/script.js
+++ js/script.js
@@ -4,5 +4,16 @@ const timerSolvePuzz = 500; //500ms
 /*---- app's state (variables) ----*/
 
 /*---- cached element references ----*/
+const instruct = document.getElementById("instruct");
+const p1name = document.getElementById("p1name");
+const p2name = document.getElementById("p2name");
+const p1points = document.getElementById("p1points");
+const p2points = document.getElementById("p2points");
+const p1solve = document.getElementById("p1solve");
+const p2solve = document.getElementById("p2solve");
+const gameboard = document.getElementById("gameboard");
+const letters = document.getElementById("letters");
+const spinner = document.getElementById("spinner");
+const spinbutt = document.getElementById("spinbutt");
 /*---- event listeners ----*/
 /*---- functions ----*/
