ca615389072617b7638be2fdffa8b3bcd29a567f
Randolph <randolph@Randolphs-Mini.home>
2022-08-09 11:18:35 -0500

    event listnere wrong placeholder save

--- js/script.js
+++ js/script.js
@@ -1,81 +1,34 @@
 /*---- constants ----*/
 import { words, lettersArray } from "./library.js";
 
-// const words = [
-//   [
-//     "STAR WARS",
-//     "STORM TROOPER",
-//     `"IT'S A TRAP!"`,
-//     `"PATIENCE YOU MUST LEARN"`,
-//     `"I FIND YOUR LACK OF FAITH DISTURBING"`,
-//     "OBI-WAN KENOBI",
-//     "GENERAL GRIEVOUS",
-//     "AHSOKA TANO",
-//     `"YOUR PATH YOU MUST DECIDE"`,
-//     "BOUNTY HUNTER",
-//     "BE CAREFUL NOT TO CHOKE ON YOUR ASPIRATIONS",
-//   ],
-//   [
-//     "FOOD",
-//     "MORROCAN CHICKEN STEW",
-//     "MACARONI AND CHEESE",
-//     "DOUBLE CHOLATE LAYER CAKE",
-//     "CEDAR PLANK SALMON",
-//     "BOUILLABAISSE",
-//     "CHICKEN SHAWARA",
-//     "KUNG PAO CHICKEN",
-//     "SPICEY FRIED BASIL RICE",
-//     "MASALA DOSA",
-//     "SEAFOOD PAELLA",
-//     "BUTTERED TOAST WITH MARMITE",
-//     "SHISH KEBAB",
-//     "PASTEL DE NATA",
-//     "TOM YUM GOONG",
-//     "PEKING DUCK",
-//     "MASSAMAN CURRY",
-//   ],
-//   [
-//     "TV-SHOWS",
-//     "FRESH PRINCE OF BEL-AIR",
-//     "BREAKING BAD",
-//     "WHEEL OF FORTUNE",
-//     "HOUSE OF CARDS",
-//     "THE SOPRANOS",
-//     "ARRESTED DEVELOPMENT",
-//     "CURB YOUR ENTHUSIASM",
-//     "ALL IN THE FAMILY",
-//     "SONS OF ANARCHY",
-//     "I LOVE LUCY",
-//     "BATTLESTAR GALACTICA",
-//     `IT'S ALWAYS SUNNY IN PHILADELPHIA`,
-//     "FREAKS AND GEEKS",
-//     "DOCTOR WHO",
-//     `LATE NIGHT WITH CONAN O'BRIEN`,
-//     "FAWLTY TOWERS",
-//     "BUFFY THE VAMPIRE SLAYER",
-//     "GAME OF THRONES",
-//     "JERSEY SHORE",
-//   ],
-// ];
-
+const spinnerArr = [
+  100,
+  150,
+  200,
+  250,
+  300,
+  350,
+  500,
+  "LOSE TURN",
+  "LOSE POINTS",
+];
 const numOfRounds = 3;
 const timerSolvePuzz = 500; //500ms
 /*---- app's state (variables) ----*/
 let round;
-let p1Name;
-let p1Points;
-let p2Name;
-let p2Points;
+let player;
 let instruct;
 let trackerArr;
-let compareArr;
+let compareCharArr; //array to compare guessed letter with letters in word
 let currentWord;
 let playerchoice;
 let word;
 let p1Solve;
 let p2Solve;
 let theme;
-
+let currentPlayer;
+let spinResult = 0;
+let targetLetter;
 /*---- cached element references ----*/
 const roundEl = document.getElementById("round");
 const instructEl = document.getElementById("instruct");
@@ -90,6 +43,7 @@ const lettersEl = document.getElementById("letters");
 const spinnerEl = document.getElementById("spinner");
 const spinButtEl = document.getElementById("spinbutt");
 const themeEl = document.getElementById("theme");
+const spin1El = document.getElementById("spin1");
 
 /*---- event listeners ----*/
 // p1SolveEl.addEventListener("keypress");
@@ -102,20 +56,44 @@ init();
 render();
 newRound(words);
 render();
-/*---- functions ----*/
+spinButtEl.addEventListener("click", spinIt);
+
+/*---- FUNCTIONS ----*/
+function checkLetter() {}
 function init() {
   round = 0;
-  p1Name = "Player 1";
-  p1Points = 0;
-  p2Name = "Player 2";
-  p2Points = 0;
+  player = [
+    {
+      name: "Player 1",
+      points: 0,
+    },
+    {
+      name: "Player 2",
+      points: 0,
+    },
+  ];
+
   instruct = `Let's get ready to play!!
     Please enter your names in the prompt boxes`;
   trackerArr = [];
-  compareArr = [];
+  compareCharArr = [];
   p1Solve = "SOLVE";
   p2Solve = "SOLVE";
   theme = "The word or phrase hint will appear here";
+  spin1El.textContent = "0";
+  currentPlayer = player[0];
+}
+function render() {
+  roundEl.textContent = `Round ${round} of ${numOfRounds}`;
+  instructEl.textContent = instruct;
+  p1NameEl.textContent = player[0].name;
+  p2NameEl.textContent = player[1].name;
+  p1PointsEl.textContent = `${player[0].points}`;
+  p2PointsEl.textContent = `${player[1].points}`;
+  themeEl.textContent = theme;
+  lettersEl;
+  spin1El.textContent = spinResult;
+  spinButtEl;
 }
 function newRound(wordArr) {
   round = round + 1;
@@ -137,18 +115,23 @@ function generateBoards(selectedWord) {
   spltphrase.forEach((word, i) => {
     spltword[i] = word.split("");
   });
-  console.log(spltword);
+  // console.log(spltword);
   //create div containers for word characters
   for (let i = 0; i < spltphrase.length; i++) {
     wordDivContainer[i] = document.createElement("div");
     wordDivContainer[i].classList.add("wordContainer");
     gameBoardEl.append(wordDivContainer[i]);
+    //create large character array of all letters and symbols without spaces to track letter selection to
+    compareCharArr.push(...spltphrase[i]);
+    trackerArr.push(...spltphrase[i]);
+
     for (let j = 0; j < spltword[i].length; j++) {
-      count = count + 1;
       charDivContainer[j] = document.createElement("div");
-      charDivContainer[j].id = `gbLetter${count}`;
+      charDivContainer[j].id = count;
       charDivContainer[j].classList.add("letterContainer");
+      // charDivContainer[j].textContent = spltword[i][j];
       wordDivContainer[i].append(charDivContainer[j]);
+      count = count + 1;
       //Show characters in the squares if not letters of alphabet
       if (
         spltword[i][j] === "'" ||
@@ -165,6 +148,7 @@ function generateBoards(selectedWord) {
         charDivContainer[j].textContent = spltword[i][j];
       }
     }
+    //create array with charcters AND spaces for comparing
   }
   // generate letters to choose from and add to letters container
   lettersArray.forEach(function (letter, i) {
@@ -175,20 +159,6 @@ function generateBoards(selectedWord) {
     lettersEl.append(letterDivContainer[i]);
   });
 }
-function render() {
-  roundEl.textContent = `Round ${round} of ${numOfRounds}`;
-  instructEl.textContent = instruct;
-  p1NameEl.textContent = p1Name;
-  p2NameEl.textContent = p2Name;
-  p1PointsEl.textContent = `${p2Points}`;
-  p2PointsEl.textContent = `${p2Points}`;
-  p1SolveEl.textContent = p1Solve;
-  p2SolveEl.textContent = p2Solve;
-  themeEl.textContent = theme;
-  lettersEl;
-  spinnerEl;
-  spinButtEl;
-}
 function randomWord(wordArr) {
   let themeIndex = Math.floor(Math.random() * wordArr.length);
   let wordIndex =
@@ -196,15 +166,53 @@ function randomWord(wordArr) {
   //return array that contains [theme,word]
   return [wordArr[themeIndex][0], wordArr[themeIndex][wordIndex]];
 }
+//returns a random element from an array and adjusts player totals
+function spinIt() {
+  spinResult = spinnerArr[randomNumberGen(spinnerArr.length - 1, 0)];
+  render();
+  if (spinResult === "LOSE TURN") {
+    switchPlayer();
+  } else if (spinResult === "LOSE POINTS") {
+    currentPlayer.points = 0;
+    switchPlayer();
+  } else if (spinResult !== "LOSE TURN" || spinResult !== "LOSE POINTS") {
+    instruct = `${currentPlayer.name}, choose a letter or click solve to solve the puzzle`;
+    render();
+    //deactivate spinner button so that player can't spin again.
+    spinButtEl.removeEventListener("click", spinIt);
+    //activate letters button so player can choose letter.
+    lettersEl.addEventListener("click", function (e) {
+      checkBoard(e.target.id);
+      console.log(trackerArr);
+    });
+  }
+}
+function randomNumberGen(highNum, lowNum) {
+  return Math.floor(Math.random() * (highNum - lowNum + 1)) + lowNum;
+}
+function switchPlayer() {
+  if (currentPlayer === player[0]) {
+    currentPlayer = player[1];
+    instruct = `${player[1].name} Hit the SPIN IT!! <spacebar> or click SOLVE <z>`;
+  } else if (currentPlayer === player[1]) {
+    currentPlayer = player[0];
+    instruct = `${player[0].name} Hit the SPIN IT!! <spacebar> or click SOLVE <z>`;
+  }
+}
+function checkBoard(letter) {
+  compareCharArr.forEach(function (char, i) {
+    if (char === letter) {
+      currentPlayer.points = currentPlayer.points + spinResult;
+      document.getElementById(i).textContent = letter;
+      trackerArr.splice(i, 1);
+      render();
+    }
+  });
+}
 function randomPlayerSelect() {}
 function correctLetter() {}
 function wrongLetter() {}
-function switchPlayer() {}
-function checkLetter() {}
-function updateTrackerArr() {}
 function endRoundCheck() {}
-function randomPoints() {}
 function revealBoardLetter() {}
-function createGameBoard() {} //create gameboard based on random selection of theme and phrase
 function disableLetter() {}
 function switchbutton() {} //tobble solve buttons to buzzers
